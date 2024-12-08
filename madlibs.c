7506029e8693600e7c751c4d1591eb04651d4bd6
Zion Gomez <ziong@unr.edu>
2024-12-07 16:49:40 -0800

    added all functions together, debugged, and reqrote/edited so that all functions worked together. madlibs.c written

--- madlibs.c
+++ madlibs.c
@@ -1,44 +1,87 @@
 //Sophia Koshy
 //Final Group Project
 
-#include<stdio.h>
+#include <stdio.h>
 #define FILENAME "madlib2.txt"
-#define LINE_SIZE 200  
-#define NUM_LINES 20
+#define SIZE 200  
+#define SIZE2 500
 
-void readFile(FILE*fin);
+void readFile(FILE* fin, char fileString[]);
+void enteredWord(char wordType[], char userWord[]);
+void appendUserString(int* index, char userWord[], char fileString[]);
+void appendFileString(int* index, char temp[], char fileString[]);
 
 int main(){
 
-	FILE*fin;
+	FILE* fin;
+    char fileString[SIZE2];
+
 	fin = fopen(FILENAME,"r");
+    
 	if (fin == NULL){
-		printf("Could not open file.\n");
-		return 0;
-	}
-	readFile(fin);
+        printf("Could not open file.\n");
+        return 0;
+    }
 
-return 0;
+    readFile(fin, fileString);
+    printf("%s\n", fileString);
+
+    return 0;
 }
 
-void readFile(FILE*fin){
-	char line[200];
-	while(fgets(line,LINE_SIZE,fin)){
-		if(line[1]=='\n'){
-			printf("Found\n");
-			//userInputArray();
-		}
-	
-	}
+void readFile(FILE* fin, char fileString[]){
+    int index = 0;
+    char temp[SIZE];
+    char userWord[SIZE];
+
+    while(fgets(temp, SIZE, fin) != NULL){
+        switch(temp[1]){
+            case '\n':
+                enteredWord(temp, userWord);
+                appendUserString(&index, userWord, fileString);
+                break;
+            default:
+                appendFileString(&index, temp, fileString);
+                break;
+        }
+    }
+    fileString[index] = '\0';
 }
 
+void enteredWord(char wordType[], char userWord[]){
+    switch(wordType[0]){
+        case 'A':
+            printf("Enter an adjective: ");
+            break;
+        case 'N':
+            printf("Enter a noun: ");
+            break;
+        case 'V':
+            printf("Enter a verb: ");
+            break;
+    }
 
+    scanf("%s", userWord);
+}
 
+void appendUserString(int* index, char userWord[], char fileString[]){
+    fileString[*index] = ' ';
+    *index += 1;
 
+    for(int i = 0; userWord[i] != '\0'; i++){
+        fileString[*index] = userWord[i];
+        *index += 1;
+    }
+}
 
+void appendFileString(int* index, char temp[], char fileString[]){
+    if(*index != 0 && temp[0] != '.'){
+        fileString[*index] = ' ';
+        *index += 1;
+    }
 
-
-
-
-
-
+    for(int i = 0; temp[i] != '\n'; i++){
+        fileString[*index] = temp[i];
+        *index += 1;
+    }
+}
