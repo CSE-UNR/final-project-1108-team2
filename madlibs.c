3a08c268eac6adb3c52904873e3337c6fa11931b
Zion Gomez <ziong@unr.edu>
2024-12-10 15:27:12 -0800

    added 5th function, lookForNAV

--- madlibs.c
+++ madlibs.c
@@ -7,21 +7,17 @@
 #define FILENAME "madlib2.txt"
 #define SIZE 200  
 
-void readFile(FILE* fin, char fileString[][SIZE]);
+void readFile(FILE* fin, char arrayNAV[], char fileString[][SIZE]);
 void enteredWord(char wordType, char userWord[]);
 void replaceWithUserString(char arrayNAV[], char fileString[][SIZE]);
-void storeNAV(char arrayNAV[], char fileString[][SIZE]);
 void displayMadlib(char fileString[][SIZE]);
+int lookForNAV(char fileString[][SIZE], char wordType);
 
 int main(){
 
     FILE* fin;
     char fileString[SIZE][SIZE];
     char arrayNAV[SIZE];
-
-    arrayNAV[0] = 'N';
-    arrayNAV[1] = 'V';
-    arrayNAV[2] = '\0';
 
     fin = fopen(FILENAME,"r");
 
@@ -30,28 +26,40 @@ int main(){
         return 0;
     }
 
-    readFile(fin, fileString);
-    //storeNAV(arrayNAV, fileString);
-    replaceWithUserString(arrayNAV, fileString);
-    displayMadlib(fileString);
+    readFile(fin, arrayNAV, fileString);
+
     fclose(fin);
+
+    replaceWithUserString(arrayNAV, fileString);
+
+    displayMadlib(fileString);
+
     return 0;
 }
 
-void readFile(FILE* fin, char fileString[][SIZE]){
-    int index = 0;
+void readFile(FILE* fin, char arrayNAV[], char fileString[][SIZE]){
+    int fileIndex = 0;
+    int arrayNAVIndex = 0;
     char temp[SIZE];
 
     while(fgets(temp, SIZE, fin) != NULL){
         for(int i = 0; temp[i] != '\n'; i++){
-            fileString[index][i] = temp[i];
-            if(temp[i+1] == '\n' || temp[i+1] == '\0'){
-                fileString[index][i+1] = '\0';
+            switch(temp[i+1]){
+                case '\n':
+                    fileString[fileIndex][i+1] = '\0';
+
+                    if((temp[i] == 'N' || temp[i] == 'A' || temp[i] == 'V') && i == 0){
+                        arrayNAV[arrayNAVIndex] = temp[i];
+                        arrayNAVIndex++;
+                    }
+                default:
+                    fileString[fileIndex][i] = temp[i];
+                    break;
             }
         }
-        index++;
+        fileIndex++;
     }
-
+    arrayNAV[arrayNAVIndex] = '\0';
 }
 
 void enteredWord(char wordType, char userWord[]){
@@ -75,24 +83,32 @@ void replaceWithUserString(char arrayNAV[], char fileString[][SIZE]){
 
     for(int i = 0; arrayNAV[i] != '\0'; i++){
         enteredWord(arrayNAV[i], userWord);
-        bool foundNAV = 0;
 
-        for(int j = 0; foundNAV != 1; j++){
-            if(fileString[j][0] == arrayNAV[i] && fileString[j][1] == '\0'){
-                foundNAV = 1;
+        int fRow = lookForNAV(fileString, arrayNAV[i]);
 
-                for(int c = 0; userWord[c] != '\0'; c++){
-                    fileString[j][c] = userWord[c];
+        for(int c = 0; userWord[c] != '\0'; c++){
+            fileString[fRow][c] = userWord[c];
 
-                    if(userWord[c+1] == '\0'){
-                        fileString[j][c+1] = '\0';
-                    }
-                }
+            if(userWord[c+1] == '\0'){
+                fileString[fRow][c+1] = '\0';
             }
         }
     }
 }
 
+int lookForNAV(char fileString[][SIZE], char wordType){
+    bool foundNAV = 0;
+    int index = 0;
+
+    for(int j = 0; foundNAV != 1; j++){
+        if(fileString[j][0] == wordType && fileString[j][1] == '\0'){
+            foundNAV = 1;
+            index = j;
+        }
+    }
+
+    return index;
+}
 
 void displayMadlib(char fileString[][SIZE]){
     for(int i = 0; fileString[i][0] != '\0'; i++){
