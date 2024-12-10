4b2831130f2a8bc7d8ec7704dd1134a0b1ee5881
Zion Gomez <ziong@unr.edu>
2024-12-10 02:35:35 -0800

    replaceWithUserInput function complete, test cases with arrayNAV, and changes to displayMadlib

--- madlibs.c
+++ madlibs.c
@@ -3,6 +3,7 @@
 //Date: 12/10/2024
 
 #include <stdio.h>
+#include <stdbool.h>
 #define FILENAME "madlib1.txt"
 #define SIZE 200  
 
@@ -16,6 +17,11 @@ int main(){
 
     FILE* fin;
     char fileString[SIZE][SIZE];
+    char arrayNAV[SIZE];
+
+    arrayNAV[0] = 'N';
+    arrayNAV[1] = 'V';
+    arrayNAV[2] = '\0';
 
     fin = fopen(FILENAME,"r");
 
@@ -25,6 +31,7 @@ int main(){
     }
 
     readFile(fin, fileString);
+    replaceWithUserString(arrayNAV, fileString);
     displayMadlib(fileString);
     fclose(fin);
     return 0;
@@ -37,6 +44,9 @@ void readFile(FILE* fin, char fileString[][SIZE]){
     while(fgets(temp, SIZE, fin) != NULL){
         for(int i = 0; temp[i] != '\n'; i++){
             fileString[index][i] = temp[i];
+            if(temp[i+1] == '\n' || temp[i+1] == '\0'){
+                fileString[index][i+1] = '\0';
+            }
         }
         index++;
     }
@@ -60,14 +70,35 @@ void enteredWord(char wordType, char userWord[]){
 }
 
 void replaceWithUserString(char arrayNAV[], char fileString[][SIZE]){
-    for(int i = 0; arrayNAV[i] != '\0'; i++){
+    char userWord[SIZE];
 
+    for(int i = 0; arrayNAV[i] != '\0'; i++){
+        enteredWord(arrayNAV[i], userWord);
+        bool foundNAV = 0;
+
+        for(int j = 0; foundNAV != 1; j++){
+            if(fileString[j][0] == arrayNAV[i]){
+                foundNAV = 1;
+
+                for(int c = 0; userWord[c] != '\0'; c++){
+                    fileString[j][c] = userWord[c];
+
+                    if(userWord[c+1] == '\0'){
+                        fileString[j][c+1] = '\0';
+                    }
+                }
+            }
+        }
     }
 }
 
 
 void displayMadlib(char fileString[][SIZE]){
     for(int i = 0; fileString[i][0] != '\0'; i++){
-        printf("%s ", fileString[i]);
+        printf("%s", fileString[i]);
+        if(fileString[i+1][0] != '\0' && fileString[i+1][0] != ',' && fileString[i+1][0] != '!' && fileString[i+1][0] != '.' && fileString[i+1][0] != ' '){
+            printf(" ");
+        }
     }
+    printf("\n");
 }
